33e4b7589de37335fa3d467b9891bd71c818c728
zBlueOrangePie <100342472+zBlueOrangePie@users.noreply.github.com>
2026-03-29 11:25:12 +0800

    Update interactionCreate.js

--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -2,9 +2,11 @@ const { Events, MessageFlags } = require("discord.js");
 const { errorEmbed1, errorEmbed2 } = require("../utils/embedHandler.js");
 const { handleNpButton } = require("../utils/npButtonUtils.js");
 const { handleQueueButton } = require("../utils/queueButtonUtils.js");
+const { handleSearchButton } = require("../utils/searchButtonUtils.js");
 
 const NP_BUTTON_IDS = ["np_pause_resume", "np_skip", "np_stop", "np_repeat", "np_autoplay"];
 const QUEUE_BUTTON_PREFIX = "queue_";
+const SEARCH_BUTTON_PREFIX = "search_track_";
 
 module.exports = {
     name: Events.InteractionCreate,
@@ -21,6 +23,10 @@ module.exports = {
 
             if (id.startsWith(QUEUE_BUTTON_PREFIX) && !id.startsWith("queue_page_indicator")) {
                 return handleQueueButton(interaction, client);
+            }
+
+            if (id.startsWith(SEARCH_BUTTON_PREFIX) || id === "search_track_all") {
+                return handleSearchButton(interaction, client);
             }
 
             return;
@@ -42,15 +48,14 @@ module.exports = {
             if (interaction.replied || interaction.deferred) {
                 await interaction.followUp({
                     embeds: [errorEmbed1],
-                    flags: MessageFlags.Ephemeral,
+                    flags:  MessageFlags.Ephemeral,
                 });
             } else {
                 await interaction.reply({
                     embeds: [errorEmbed2],
-                    flags: MessageFlags.Ephemeral,
+                    flags:  MessageFlags.Ephemeral,
                 });
             }
         }
     },
 };
-              
